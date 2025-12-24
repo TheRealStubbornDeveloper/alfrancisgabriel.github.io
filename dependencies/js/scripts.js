d4415854e09f2c6cdde62b7d3dba48f0aa2d3cde
StubbornVision Dev <stubborndev@gmail.com>
2025-12-24 15:37:39 +0800

    Fixed console errors: replaced jQuery with vanilla JS, removed unused Twitter image from meta, updated button ID and class for navbar shrink

--- dependencies/js/scripts.js
+++ dependencies/js/scripts.js
@@ -1,58 +1,47 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-    */
+    /*!
+     * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
+     * Copyright 2013-2020 Start Bootstrap
+     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+     */
     (function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
+    // Smooth scrolling using vanilla JS
+    document.querySelectorAll('a[href*="#"]:not([href="#"])').forEach(function(anchor) {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const target = document.getElementById(targetId);
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
             }
-        }
+        });
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
+    document.querySelectorAll('.js-scroll-trigger').forEach(function(anchor) {
+        anchor.addEventListener('click', function() {
+            const mobileMenu = document.getElementById('mobileMenu');
+            if (mobileMenu) {
+                mobileMenu.style.display = 'none';
+            }
+        });
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
+    // Navbar shrink on scroll
+    window.addEventListener('scroll', function() {
+        const nav = document.getElementById('mainNav');
+        if (window.pageYOffset > 50) {
+            nav.classList.add('bg-gray-900');
         } else {
-            $("#mainNav").removeClass("navbar-shrink");
+            nav.classList.remove('bg-gray-900');
         }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    });
+
+
 
     // Fade in sections on scroll
     $(window).on('scroll', function() {
